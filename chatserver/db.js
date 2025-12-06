d0284df3571e709b7ec8521d10316281a6d21c9f
Ok Junseo <dhrwnstj@naver.com>
2025-12-06 21:03:09 +0900

    디벨로핑 마이그레이션

--- chatserver/db.js
+++ chatserver/db.js
@@ -10,7 +10,7 @@ const mysql = require("mysql2/promise");
 // } = process.env;
 
 // RDS 접속 정보 하드코딩 (지금 환경에서는 이게 제일 단순)
-const DB_HOST = "chat-mysql.cleqg0wcir2i.ap-northeast-2.rds.amazonaws.com";
+const DB_HOST = "chat-mysql.crjpe7o6zkmo.us-east-1.rds.amazonaws.com";
 const DB_PORT = 3306;
 const DB_USER = "admin"; // RDS 만들 때 적은 마스터 유저
 const DB_PASSWORD = "RDS_비밀번호_여기에"; // 실제 비번으로 바꾸기
